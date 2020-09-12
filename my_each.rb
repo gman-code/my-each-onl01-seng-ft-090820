196bf77f6fb5c6ac36dd0c1506a2116b5924f0cd
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-12 13:15:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,32 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ba", "ra", "ka"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+
+    yield(array[i])
+    i = i + 1
+  end
 end
+
+my_each(list) {|i| puts "So I think #{i} its a nice day"}
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
+  
+  
+  
+  
+  
+  
+  
