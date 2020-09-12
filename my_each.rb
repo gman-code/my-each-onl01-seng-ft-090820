7aa288a4af946c3fc14447a651b80094d23e3d00
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-12 13:52:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,29 +1,5 @@
-list = ["ba", "ra", "ka"]
-
-def my_each(array)
-  i = 0
-  while i < array.length
-
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-my_each(list) {|i| puts "So I think #{i} its a nice day"}
 
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| "Hello #{i} what's up?"}
-  
   
   
   
